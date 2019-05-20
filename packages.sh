5a9011d63dcba6a9beb701db131bddbda249760f
alexPNG <41132349+alexPNG@users.noreply.github.com>
2019-05-20 20:27:36 +0100

    testing

--- packages.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-dpkg-deb -bZgzip /Users/Alexandre/Documents/github/alexpng.github.io/projects/Bodak\ Siri debs
-
-dpkg-deb -bZgzip /Users/Alexandre/Documents/github/alexpng.github.io/projects/Jarvis debs
