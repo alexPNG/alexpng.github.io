520314c728f9b333c36c7aed1da9b632cb964cf4
alexPNG <41132349+alexPNG@users.noreply.github.com>
2019-05-20 20:01:40 +0100

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 dpkg-deb -bZgzip /Users/Alexandre/Documents/github/alexpng.github.io/projects/Bodak\ Siri debs
+
+dpkg-deb -bZgzip /Users/Alexandre/Documents/github/alexpng.github.io/projects/Jarvis debs
